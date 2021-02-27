3f20345e98fc32aedfaa470211ddb1d6573b474e
Robert Hardy <robert@goattea.com>
2021-02-26 22:49:05 -0700

    Update to dotnet 5

--- PokerHands.Console/Program.cs
+++ PokerHands.Console/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace PokerHands.Console
 {
-	class Program
-	{
-		static void Main()
-		{
-			//Sample input:
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample input:
 			//Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH
 			//Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S
 			//Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH
@@ -51,8 +48,6 @@ namespace PokerHands.Console
 
 
 			System.Console.ReadLine();
-		}
-
-
-	}
+        }
+    }
 }
